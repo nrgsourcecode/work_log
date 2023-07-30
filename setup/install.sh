4f34f85fdbf04e3b1e98784fc505dd7a6c21ed97
Nikola Radovanović <admin@nrgsourcecode.com>
2023-07-30 19:49:35 +0200

    Installed user themes through terminal

--- setup/install.sh
+++ setup/install.sh
@@ -1,5 +1,13 @@
-# sudo apt install gnome-tweaks gnome-shell-extensions gnome-shell-extension-prefs
-# gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+sudo apt install gnome-tweaks gnome-shell-extensions gnome-shell-extension-prefs
+gnome_version=$(gnome-shell --version | awk '{print $3}' | awk -F'.' '{print $1}')
+user_themes=$(curl -s https://extensions.gnome.org/extension/19/user-themes/ | grep -o "data-svm=\"[^\"]*\"" | sed 's/data-svm="//;s/"$//' | grep -o "&quot;$gnome_version&quot;: {&quot;pk&quot;: [^}]*}" | grep -o '&quot;version&quot;: [0-9]*' | grep -o '[0-9]\+')
+url="https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v$user_themes.shell-extension.zip"
+user_themes_uuid=user-theme@gnome-shell-extensions.gcampax.github.com
+temp_path="/tmp/$user_themes_uuid.zip"
+wget "$url" -O "$temp_path"
+gnome-extensions install --force "$temp_path"
+gnome-extensions enable $user_themes_uuid
+killall -HUP gnome-shell
 
 setup_folder=$(realpath "$(dirname "$0")")
 current_folder=$(pwd)
@@ -36,4 +44,3 @@ create_user_theme work-log-error
 replace_text_in_file "background-color: #131313" "background-color: #ff0000" ~/.themes/work-log-error/gnome-shell/gnome-shell.css
 
 source $setup_folder/install_service.sh
-echo "Press ALT+F2 and run command r to restart the shell"
